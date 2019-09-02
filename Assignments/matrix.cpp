58667d8e30e78e8d66f25b3e03d62b32ac6c863d
Nicolas Perez Roa <nicolasipr.97@gmail.com>
2019-09-02 12:45:05 -0400

    Creating NxM matrix and filled with pseudo unsigned Random numbers

---
+++ Assignments/matrix.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+// due date:  October First
+using namespace std;
+
+#define MAXN 100
+#define MAXM 100
+
+
+vector < vector < unsigned long long int > > Matrix(MAXN, vector<unsigned  long long int>(MAXM,0));
+
+unsigned long long int pacing(long long int a, long long int b){
+
+    return abs(a - b);
+}
+
+bool itsContained(int   i, int  j){
+
+    if(Matrix[i][j]) return true;
+    else return false;
+}
+
+void walkingThrough(){
+
+
+
+}
+
+
+int main(){
+
+    int N = 0, M = 0; cin >> N >> M;
+
+    Matrix.resize(N, vector<unsigned long long int>(M));
+
+
+    //              Pseudo Random Generator
+
+    random_device random_device;
+    mt19937 random_engine(random_device());
+    uniform_int_distribution<int> distribution_1_1000(1, 9);
+
+
+    for(auto i =  0; i < N; i++){
+        for(auto j =  0; j  < M; j++){
+            Matrix[i][j] = distribution_1_1000(random_engine);
+        }
+        cout << endl;
+    }
+    for(auto i =  0; i < N; i++){
+        for(auto j =  0; j  < M; j++){
+            cout << Matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+   
+
+
+    return 0;
+}
---
+++ Sorting/README.md
@@ -0,0 +1,20 @@
+# About this repository 
+Not much for now, just trying to get involved with some algorithms ideas and implementations through. And obviously 
+its data structures.  
+
+## Folder Organization
+
+    ├── ...
+    ├── Sorting                  # Test files (alternatively `spec` or `tests`)
+    │   ├──              # Load and stress tests
+    │   ├── ... 
+    ├── README.md
+    └── ...
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
